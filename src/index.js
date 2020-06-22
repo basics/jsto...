598a02deaf259e5ec4a1abf312a4c4501b500948
Stefan Eckert <stefan.eckert@sportalliance.com>
2020-06-22 18:12:42 +0200

    feat(index): runtime type check for function arguments and classes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,149 @@
-// eslint-disable-next-line no-unused-vars
-export function typ(c) {
-  return undefined;
+
+const TYPED_FUN = Symbol('typed function');
+const INNER_DATA = Symbol('inner data');
+
+let activeArgList;
+let activeArgIndex;
+
+export function typ(typeDesc) {
+  const { type, def } = extractType(typeDesc);
+  if (!type) {
+    throw new Error('no type assigned');
+  }
+  if (activeArgList) {
+    let arg = activeArgList[activeArgIndex];
+    activeArgIndex += 1;
+    if (activeArgIndex === activeArgList.length) {
+      activeArgList = undefined;
+      activeArgIndex = undefined;
+    }
+    checkType(arg, type);
+
+    return arg;
+  }
+  return def;
 }
 
-// eslint-disable-next-line no-unused-vars
 export function fun(type, func) {
-  return undefined;
+  if (func === undefined) {
+    func = type;
+    type = undefined;
+  }
+  function f(...args) {
+    activeArgList = args;
+    activeArgIndex = 0;
+    const result = func.call(this);
+    activeArgList = undefined;
+    checkType(result, type);
+    return result;
+  }
+  f[TYPED_FUN] = true;
+  return f;
 }
 
-// eslint-disable-next-line no-unused-vars
-export function cls(prototype) {
-  return undefined;
+export function cls(classDesc) {
+  const { constructor, ...proto } = classDesc;
+
+  const types = {};
+  const prototype = {};
+  Object.entries(proto).forEach(([key, val]) => {
+    if (val[TYPED_FUN]) {
+      prototype[key] = val;
+      return;
+    }
+    const { type } = extractType(val);
+    types[key] = type;
+
+    Object.defineProperty(prototype, key, {
+      get() {
+        return getInner(this, key);
+      },
+      set(value) {
+        checkType(value, types[key]);
+        return setInner(this, key, value);
+      }
+    });
+  });
+  function con(...args) {
+    constructor.apply(this, args);
+  }
+  con.prototype = prototype;
+
+  return con;
+}
+
+function extractType(type) {
+  if (type === undefined || type === null) {
+    throw new Error('no type assigned');
+  }
+  const to = typeof type;
+  if (to === 'function') {
+    return {
+      type,
+      def: undefined
+    };
+  }
+  if (to === 'number') {
+    return {
+      type: Number,
+      def: type
+    };
+  }
+  if (to === 'string') {
+    return {
+      type: String,
+      def: type
+    };
+  }
+  if (to === 'boolean') {
+    return {
+      type: Boolean,
+      def: type
+    };
+  }
+  if (type.constructor) {
+    return {
+      type: type.constructor,
+      def: type
+    };
+  }
+  throw new Error(`no type found ${type}`);
+}
+
+function checkType(type, expected) {
+  if (expected === undefined) {
+    if (type !== undefined) {
+      throw new Error(`void function doesn't allow results ${type}`);
+    }
+    return;
+  }
+  const to = typeof type;
+  if (expected === String && to === 'string') {
+    return;
+  }
+  if (expected === Number && to === 'number') {
+    return;
+  }
+  if (expected === Boolean && to === 'boolean') {
+    return;
+  }
+  throw new Error(`${expected} function doesn't allow results ${type}`);
+}
+
+function getInner(obj, key) {
+  const data = obj[INNER_DATA];
+  if (!data) {
+    return;
+  }
+  return data[key];
+}
+
+function setInner(obj, key, value) {
+  let data = obj[INNER_DATA];
+  if (!data) {
+    data = {};
+    obj[INNER_DATA] = data;
+  }
+  data[key] = value;
+  return true;
 }
--- test/index.js
+++ test/index.js
@@ -1,10 +1,142 @@
-// import { assert } from 'chai';
+import { assert } from 'chai';
 import { typ, fun, cls } from '../src';
 
-describe('test.', () => {
-  it('should exist', () => {
-    typ();
-    fun();
-    cls();
+describe('type tests', () => {
+
+  class Test {
+
+  }
+
+  it('should work with valid number declarations', () => {
+    let nr1 = typ(Number);
+
+    assert.equal(nr1, undefined);
+
+    nr1 = 5;
+
+    let nr2 = typ(5);
+
+    assert.equal(typeof nr1, 'number');
+    assert.equal(typeof nr2, 'number');
   });
+
+  it('should work with valid string declarations', () => {
+    let str1 = typ(String);
+
+    assert.equal(str1, undefined);
+
+    str1 = 'hello';
+
+    let str2 = typ('hello');
+
+    assert.equal(typeof str1, 'string');
+    assert.equal(typeof str2, 'string');
+  });
+
+  it('should work with valid type declarations', () => {
+    let h1 = typ(Test);
+
+    assert.equal(h1, undefined);
+
+    h1 = new Test();
+
+    let h2 = typ(new Test());
+
+    assert.equal(h1 instanceof Test, true);
+
+    assert.equal(typeof h2, 'object');
+    assert.equal(h2 instanceof Test, true);
+  });
+});
+
+describe('function tests', () => {
+
+  class Test {
+
+  }
+
+  it('should work with valid function declarations', () => {
+    const fn1 = fun(String, (p = typ(Number)) => {
+      return `hallo ${p}`
+    });
+
+    const res1 = fn1(5);
+    assert.equal(res1, 'hallo 5');
+
+    assert.throws(() => {
+      res1('string instead of number');
+    });
+  });
+
+  it('should work with valid void function declarations', () => {
+    let nr;
+    const fn1 = fun((p = typ(Number)) => {
+      nr = p;
+    });
+
+    const res1 = fn1(5);
+    assert.equal(res1, undefined);
+  });
+
+  it('should not work with invalid function declarations', () => {
+    assert.throws(() => {
+      fun((p = typ(Number)) => {
+        return 'hello'
+      })();
+    });
+
+    assert.throws(() => {
+      fun(String, () => {
+        return 5;
+      })();
+    });
+  });
+
+});
+
+describe('class tests', () => {
+
+  class Test {
+
+  }
+
+  it('should work with valid class declarations', () => {
+    let Test = cls({
+      bar: Number,
+
+      constructor: fun(function () {
+        this.bar = 7;
+      }),
+
+      foo: fun(Number, function (str = typ(String)) {
+        return parseFloat(str);
+      })
+    })
+
+    const test = new Test();
+    const nr = test.foo('12');
+
+    assert.equal(test.bar, 7);
+    assert.equal(nr, 12);
+
+    assert.throws(() => {
+      test.foo(12);
+    });
+  });
+
+  it('should not work with invalid class declarations', () => {
+
+    assert.throws(() => {
+      let Test = cls({
+        bar: Number,
+
+        constructor: fun(function () {
+          this.bar = '12';
+        })
+      })
+
+      const test = new Test();
+    });
+  });
+
 });
